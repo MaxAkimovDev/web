5b18ab947616735d197d24cda813552c2188bcda
MaxAkimovDev <maksym.akimov@nure.ua>
2020-05-11 14:30:29 +0200

    icon/home

--- application/views/home.php
+++ application/views/home.php
@@ -33,7 +33,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
   <body>
   	<nav class="navbar navbar-expand-lg navbar-dark ftco_navbar bg-dark ftco-navbar-light" id="ftco-navbar">
 	    <div class="container">
-		      <a class="navbar-brand" href="<?php echo base_url(); ?>index.php/home/index"><img src="https://img.icons8.com/cotton/64/000000/trainers.png"/><!-- <span class="flaticon-pizza-1 mr-1"></span> -->Food<br><small>Runner</small></a>
+		      <a class="navbar-brand" href="<?php echo base_url(); ?>index.php/home/index"> <img src="https://img.icons8.com/color/64/000000/broken-leg.png"/> <!-- <img src="https://img.icons8.com/cotton/64/000000/trainers.png"/>-->Food<br><small>Runner</small></a>
 		      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#ftco-nav" aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
           <img src="https://img.icons8.com/plasticine/32/000000/squared-menu.png"/> Menu
 		      </button>
